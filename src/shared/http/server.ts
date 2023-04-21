ec0cbe01fe22ba708deedda49d0726cabd93fa7d
Tiago Paulino <tiagopaulino1989@gmail.comgit config user.email tiagopaulino1989@gmail.comgit config --global user.email tiagopaulino1989@gmail.com>
2023-04-21 02:20:34 -0300

    Initial Commit

---
+++ src/shared/http/server.ts
@@ -0,0 +1,33 @@
+import express, {Request,Response,NextFunction} from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '../errors/AppError';
+
+const app  = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+  (
+    error: Error,
+    request: Request,
+    response: Response,
+    next:NextFunction)=>{
+  if (error instanceof AppError){
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message})
+  }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error'
+    })
+});
+
+app.listen(port,()=> {
+  console.log(`Server running on port: ${port}`)
+})
